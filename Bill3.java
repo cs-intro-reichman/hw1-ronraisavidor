340dd5929aac854dfff73a5f8fb62e103fdb7ac2
Ron Rais Avidor <ronraisavidor@Rons-MacBook-Air.local>
2024-11-06 14:05:35 +0200

    Completed HW1

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,21 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		/// Check if exactly 4 arguments are provided - 3 names and the sum of the bill
+        if (args.length != 4) {
+            System.out.println("Please provide exactly 4 arguments: the three diners names and the sum of of the bill:");
+            return;
+        }
+
+        // Get the values from the command-line arguments
+        String person1 = args[0];
+        String person2 = args[1];   
+        String person3 = args[2];
+        int billSum = Integer.parseInt(args[3]);
+
+        //Calculating each person share from the final bill, and print the final result
+        double share = Math.ceil((double)billSum / 3);
+        System.out.printf("Dear %s, %s, and %s: please pay %.2f Shekels each.%n", 
+                          person1, person2, person3, share);
 	}
 }
--- Demo0.java
+++ Demo0.java
@@ -3,7 +3,11 @@
 public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
-        int i = 0;
-        // Complete the program's code here:
+        int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i += 1;
+        }
+        
     }
  }
